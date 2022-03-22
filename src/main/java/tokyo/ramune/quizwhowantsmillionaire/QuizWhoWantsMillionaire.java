7fab87a8d5960f6f627ceaa21bded74d0903dc9f
riyaya1528 <ramune.remonedo@gmail.com>
2022-03-23 02:24:06 +0900

    First Commit

---
+++ src/main/java/tokyo/ramune/quizwhowantsmillionaire/QuizWhoWantsMillionaire.java
@@ -0,0 +1,19 @@
+package tokyo.ramune.quizwhowantsmillionaire;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class QuizWhoWantsMillionaire extends JavaPlugin {
+
+    private static Vau
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
